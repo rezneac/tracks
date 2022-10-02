7df52fe69e34b3245a872c6e732318727c557268
rezneacanatolie <rezneacanatolie@gmail.com>
2022-10-02 02:32:50 +0100

    Init project

---
+++ src/screens/TrackDetailScreens.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const TrackDetailScreens = () => {
+  return <Text style={{ fontSize: 48 }}>Track detail Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackDetailScreens;
